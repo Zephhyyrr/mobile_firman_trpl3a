9c3aa3f18dd64bba5cf7614920a263ac7f489b4e
Firman Ardiansyah <ardiansyahfirman2004@gmail.com>
2024-09-03 12:22:43 +0700

    Add Radio Button and checkbox

--- Project1Firman/app/src/main/java/com/firman/project1firman/DuaActivity.kt
+++ Project1Firman/app/src/main/java/com/firman/project1firman/DuaActivity.kt
@@ -2,12 +2,13 @@ package com.firman.project1firman
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Spinner
-import android.widget.TextView
+import android.view.View
+import android.widget.*
 
-class DuaActivity : AppCompatActivity() {
+class DuaActivity : AppCompatActivity(),AdapterView.OnItemSelectedListener {
+    //: =>extends -> inherits
+    //, =>implements -> interface
+    val arrayKota = arrayOf<String>("Jakarta","Padang","Bandung","Pekanbaru","Semarang","Surabaya")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dua)
@@ -15,7 +16,11 @@ class DuaActivity : AppCompatActivity() {
         val kota = findViewById<EditText>(R.id.editKota)
         val harga = findViewById<EditText>(R.id.editHarga)
         val jumlah = findViewById<EditText>(R.id.editJumlah)
-        val cara = findViewById<Spinner>(R.id.spinnCara)
+        val berangkat = findViewById<Spinner>(R.id.spinBerangkat)
+        berangkat.onItemSelectedListener=this
+        val aa: ArrayAdapter<*> = ArrayAdapter<Any?>(this@DuaActivity, R.layout.spinner_style, arrayKota)
+        aa.setDropDownViewResource(R.layout.spinner_style)
+        berangkat.adapter = aa
         val trans = findViewById<TextView>(R.id.txtDetail)
         val proses = findViewById<Button>(R.id.btnProses)
 
@@ -31,5 +36,13 @@ class DuaActivity : AppCompatActivity() {
                         "\n Jumlah Tiket : $jumlahTiket " +
                         "\n Total Bayar :  $totalBayar "
         }
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+
     }
 }
