a91da2f8bbe35ddf94b3ad3102749902291a84e9
Firman Ardiansyah <ardiansyahfirman2004@gmail.com>
2024-09-05 09:42:07 +0700

    Add fitur spinner

--- Project1Firman/app/src/main/java/com/firman/project1firman/DuaActivity.kt
+++ Project1Firman/app/src/main/java/com/firman/project1firman/DuaActivity.kt
@@ -8,7 +8,8 @@ import android.widget.*
 class DuaActivity : AppCompatActivity(),AdapterView.OnItemSelectedListener {
     //: =>extends -> inherits
     //, =>implements -> interface
-    val arrayKota = arrayOf<String>("Jakarta","Padang","Bandung","Pekanbaru","Semarang","Surabaya")
+    val kotaBerangkat = arrayOf<String>("Jakarta","Padang","Bandung","Pekanbaru","Semarang","Surabaya")
+    lateinit var rbBayar:RadioButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dua)
@@ -16,30 +17,58 @@ class DuaActivity : AppCompatActivity(),AdapterView.OnItemSelectedListener {
         val kota = findViewById<EditText>(R.id.editKota)
         val harga = findViewById<EditText>(R.id.editHarga)
         val jumlah = findViewById<EditText>(R.id.editJumlah)
+        val bagasi = findViewById<CheckBox>(R.id.chkBagasi)
+        val executive = findViewById<CheckBox>(R.id.chkExecutive)
+        val makanan = findViewById<CheckBox>(R.id.chkMakanan)
+        val rgBayar = findViewById<RadioGroup>(R.id.rgBayar)
         val berangkat = findViewById<Spinner>(R.id.spinBerangkat)
-        berangkat.onItemSelectedListener=this
-        val aa: ArrayAdapter<*> = ArrayAdapter<Any?>(this@DuaActivity, R.layout.spinner_style, arrayKota)
-        aa.setDropDownViewResource(R.layout.spinner_style)
-        berangkat.adapter = aa
+        berangkat.onItemSelectedListener = this
+        //val aa: ArrayAdapter<*> = ArrayAdapter<Any?>(this@DuaActivity, R.layout.spinner_style, kotaBerangkat)
         val trans = findViewById<TextView>(R.id.txtDetail)
         val proses = findViewById<Button>(R.id.btnProses)
+        val aa = ArrayAdapter(this@DuaActivity, R.layout.spinner_style, kotaBerangkat)
+        aa.setDropDownViewResource(R.layout.spinner_style)
+        berangkat.adapter = aa
+
 
         proses.setOnClickListener {
             val kotaTujuan = kota.text.toString()
             val hargaTiket = harga.text.toString().toDouble()
             val jumlahTiket = jumlah.text.toString().toInt()
-            val totalBayar = hargaTiket*jumlahTiket
+            val biayaTambahan = StringBuffer()
+            var totalTambahan:Int=0
+            if(bagasi.isChecked){
+                biayaTambahan.append("\n\t\tBagasi Rp.200000")
+                totalTambahan +=200000
+            }
+            if(executive.isChecked){
+                biayaTambahan.append("\n\t\tExecutive Lounge Rp.800000")
+                totalTambahan +=800000
+            }
+            if(makanan.isChecked){
+                biayaTambahan.append("\n\t\tMakanan Rp.100000")
+                totalTambahan +=100000
+            }
+            biayaTambahan.append("\nTotal tambahan " + totalTambahan)
+            val selectedRadio:Int = rgBayar.checkedRadioButtonId
+            rbBayar = findViewById (selectedRadio)
+            val totalBayarTiket = hargaTiket*jumlahTiket
+            val totalKeseluruhan = totalTambahan + totalBayarTiket
             trans.text =
                 "Detail Transaksi : \n" +
-                        "\n Kota Tujuan: $kotaTujuan " +
-                        "\n Harga : $hargaTiket " +
+                        "\n Kota Tujuan: $kotaTujuan" +
+                        "\n Harga Tiket : $hargaTiket " +
                         "\n Jumlah Tiket : $jumlahTiket " +
-                        "\n Total Bayar :  $totalBayar "
+                        "\n Biaya Tambahan : ${biayaTambahan.toString()}" +
+                        "\n Total Bayar :  $totalBayarTiket " +
+                        "\n Metode Pembayaran : "+ rbBayar.text.toString() +
+                        "\n Biaya Keseluruhan : $totalKeseluruhan"
         }
     }
 
     override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-
+        val pilihKotaBerangkat:String = parent?.getItemAtPosition(position).toString()
+        Toast.makeText( this@DuaActivity,"Kota Keberangkatan : $pilihKotaBerangkat ", Toast.LENGTH_LONG).show()
     }
 
     override fun onNothingSelected(parent: AdapterView<*>?) {
