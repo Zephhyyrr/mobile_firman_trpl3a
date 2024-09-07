db8f1618e2a568d4885faac720a974f19f234d31
Firman Ardiansyah <ardiansyahfirman2004@gmail.com>
2024-09-07 21:33:29 +0700

    add new activity

---
+++ Project1Firman/app/src/main/java/com/firman/project1firman/EmpatActivity.kt
@@ -0,0 +1,11 @@
+package com.firman.project1firman
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EmpatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_empat)
+    }
+}
---
+++ Project1Firman/app/src/main/java/com/firman/project1firman/TigaActivity.kt
@@ -0,0 +1,11 @@
+package com.firman.project1firman
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TigaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tiga)
+    }
+}
